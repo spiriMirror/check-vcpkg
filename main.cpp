8bf16d6e5c17be577c1d29fde873d405ed16145f
MuGdxy <lxy819469559@gmail.com>
2025-10-08 22:30:41 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <Octree/octree.h>
+#include <ftetwild/ftetwild.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    ftetwild::FtetWild ftw;
     return 0;
 }
