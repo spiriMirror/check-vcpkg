6ceacf6383f6a8d75473ea662872b313e1e401c6
MuGdxy <lxy819469559@gmail.com>
2025-10-10 10:53:36 +0800

    wait to update sha512

--- local-ports/Octree/portfile.cmake
+++ local-ports/Octree/portfile.cmake
@@ -2,11 +2,9 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "SpiriMirror/Octree"
     REF "${VERSION}"
-    SHA512 56d10d535c0b427cffdddaf5a17047b3beb63296ed2a587973a6b017e5248b0b3b89ba0e4a5f84cfc9ce3de3ce01e7dcacece99e246bf7247649c87a071cf755
-    HEAD_REF mini20
+    SHA512 0
+    HEAD_REF master
 )
-
-# set(SOURCE_PATH "/Users/luxinyu/Projects/SpiriVcpkg/Octree")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
